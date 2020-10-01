cf1a046b50dbd847885deb73f252ebc5bc8b752b
mitesh5494 <mitesh5494@gmail.com>
2020-10-01 19:16:18 +0530

    first commit

---
+++ src/test/java/rahulshettyacademy/APITest.java
@@ -0,0 +1,23 @@
+package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+
+	
+	@Test
+	public void postJira()
+	{
+     System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+	
+	
+	
+	
+}
---
+++ src/test/java/rahulshettyacademy/ApiumTest.java
@@ -0,0 +1,22 @@
+package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class ApiumTest {
+
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+	
+	
+}
